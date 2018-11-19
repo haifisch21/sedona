92cb948d854a5b07d9cb943c0ea3fe11c5154208
haifisch21 <citrus29042015@icloud.com>
2018-11-19 23:02:31 +0200

    Add content
    Add fonts, add bemto, add JS. Fixed bugs

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('cause-block-first').innerHTML = '<b class="cause-block__title">НЕБОЛЬШАЯ <br> ПЛОЩАДЬ</bcause-block>' +
+    '<p class="cause-block__number">— №3 —</p>' + '<p class="cause-block__text">ВСЕ интересные места находятся очень <br> близко</p>';
+
+document.getElementById('cause-block-second').innerHTML = '<b class="cause-block__title">Красивая <br> Дорога</bcause-block>' +
+    '<p class="cause-block__number">— №4 —</p>' + '<p class="cause-block__text">ЕХАТЬ В СЕДОНУ ИЗ ЛАС-ВЕГАСА совсем <br> НЕ СКУЧНО!</p>';
+
+document.getElementById('cause-block-third').innerHTML = '<b class="cause-block__title">МАЛО <br> ТУРИСТОВ</bcause-block>' +
+    '<p class="cause-block__number">— №5 —</p>' + '<p class="cause-block__text">Большинство едет в гранд каньон и <br> толпится там</p>';
